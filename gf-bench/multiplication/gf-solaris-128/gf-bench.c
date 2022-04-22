385645daf5c248fc1ca27b934d1d7756f5d399d8
Hiroshi Nishida <nishida@asusa.net>
2022-04-22 14:12:01 -0800

    Use common files

--- gf-bench/multiplication/gf-solaris-128/gf-bench.c
+++ gf-bench/multiplication/gf-solaris-128/gf-bench.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 #include <wmmintrin.h>
 #include <sys/time.h>
+#include "common.h"
 #include "gf.h"
 #include "mt64.h"
-
-// Space allocation
-#define SPACE	640000
-
-// # of repeats
-#define REPEAT	200	// Actual repeat times is SPACE * REPEAT 
 
 // Main
 int
@@ -21,8 +17,6 @@ main(int argc, char **argv)
 	struct timeval	start, end;
 	__m128i		a, b; // Use Intel CLMUL instruction set
 	uint64_t	*r, *r1, *r2;
-	uint64_t	rand_init[4] = {UINT64_C(0xfd308), UINT64_C(0x65ab8),
-				UINT64_C(0x931cd54), UINT64_C(0x9475ea2)};
 
 	// Allocate for random numbers - Fist SPACE for r1 and second for r2
 	if ((r = (uint64_t *)malloc(SPACE * 2)) == NULL) { //
@@ -33,7 +27,7 @@ main(int argc, char **argv)
 	r2 = r + (SPACE / sizeof(uint64_t));
 
 	// Initialize random generator
-	init_by_array64(rand_init, 4);
+	init_genrand64(time(NULL));
 
 	// Input random numbers to r
 	j = SPACE * 2 / sizeof(uint64_t);
@@ -50,7 +44,8 @@ main(int argc, char **argv)
 		for (j = 0; j < SPACE / sizeof(uint64_t); j += 2) { // 
 			// Set a, b
 			b = _mm_set_epi64x(r2[j], r2[j + 1]);
-			// To avoid elimination by cc's -O2 option, input result into &r1[j]
+			// To avoid elimination by cc's -O2 option,
+			// input result into &r1[j]
 			gfmul(a, b, &r1[j]);
 		}
 	}
@@ -59,5 +54,6 @@ main(int argc, char **argv)
 	gettimeofday(&end, NULL);
 
 	// Print result
-	printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+	printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) -
+		(start.tv_sec * 1000000 + start.tv_usec)));
 }
