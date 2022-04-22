3e457211397bc629925573ca139174ae61de532d
Hiroshi Nishida <nishida@asusa.net>
2022-04-22 14:09:11 -0800

    Use common files

--- gf-bench/multiplication/gf-plank-8/gf-bench.c
+++ gf-bench/multiplication/gf-plank-8/gf-bench.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <time.h>
 #include <sys/time.h>
 #include "galois.h"
+#include "common.h"
 #include "mt64.h"
-
-// Space allocation
-#define SPACE	640000
-
-// # of repeats
-#define REPEAT	200	// Actual repeat times is SPACE * REPEAT 
 
 // Main
 int
@@ -18,38 +15,36 @@ main(int argc, char **argv)
 	// Variables
 	int		i, j;
 	struct timeval	start, end;
-	uint8_t		*a, *b, _a; // 8bit
+	uint8_t		a, *b, *c;
 	uint64_t	*r;
-	uint64_t	rand_init[4] = {UINT64_C(0xfd308), UINT64_C(0x65ab8),
-				UINT64_C(0x931cd54), UINT64_C(0x9475ea2)};
 
-	// Allocate first SPACE bytes for a and second SPACE bytes for b
-	if ((a = (uint8_t *)malloc(SPACE * 2)) == NULL) { // 8bit
+	// Allocate b and c
+	if ((b = (uint8_t *)malloc(SPACE * 2)) == NULL) {
 		perror("malloc");
 		exit(1);
 	}
-	b = a + (SPACE / sizeof(uint8_t)); // 8bit
+	c = b + (SPACE / sizeof(uint8_t)); // 8bit;
 
 	// Initialize random generator
-	init_by_array64(rand_init, 4);
+	init_genrand64(time(NULL));
 
-	// Input random numbers to a, b - Do not change this
-	j = SPACE * 2 / sizeof(uint64_t);
-	r = (uint64_t *)a;
-	for (i = 0; i < j; i++) {
+	// Input random numbers to a, b
+	a = (uint8_t)(genrand64_int64() & 0xff);
+	r = (uint64_t *)b;
+	for (i = 0; i < SPACE / sizeof(uint64_t); i++) {
 		r[i] = genrand64_int64();
 	}
-	_a = a[0];
 
 	// Start measuring elapsed time
 	gettimeofday(&start, NULL); // Get start time
 
-	// Repeat calc in GF
+	// Calculate c[j] = a * b[j]
 	for (i = 0; i < REPEAT; i++) {
-		for (j = 0; j < SPACE / sizeof(uint8_t); j++) {
+		for (j = 0; j < SPACE / sizeof(uint8_t); j++) { // 8bit
 			// Calculate in GF
-			// To avoid elimination by cc's -O2 option, input result into a[j]
-			a[j] = galois_single_multiply(_a, b[j], 8);; // 8bit
+			// To avoid elimination by cc's -O2 option,
+			// input result into c[j]
+			c[j] = galois_single_multiply(a, b[j], 8); // 8bit
 		}
 	}
 
@@ -57,5 +52,8 @@ main(int argc, char **argv)
 	gettimeofday(&end, NULL);
 
 	// Print result
-	printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+	printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) -
+		(start.tv_sec * 1000000 + start.tv_usec)));
+
+	exit(0);
 }
