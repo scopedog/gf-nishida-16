deeed8302873d9816969d4b5be2b56ba2bd7e631
Hiroshi Nishida <nishida@asusa.net>
2022-04-28 09:50:10 -0800

    Bug fix and organization
    Fix wrong usage of gf-ff-* and organize to use common files.

--- gf-bench/division/gf-ff-32/ff_benchmark.c
+++ gf-bench/division/gf-ff-32/ff_benchmark.c
@@ -16,20 +16,19 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-#include "ff_2_163.h"
-#include <sys/time.h>
+#include "ff_2_32.h"
+#include <sys/timeb.h>
 #include <stdlib.h>
 
 #define CELLS 1000
-#define ITERATIONS 2500000
+#define ITERATIONS 100000
 
 int main () {
   int i;
   int op;
-  //struct timeb tm;
-	struct timeval	start, end;
-  //long int seconds;
-  //long int milliseconds;
+  struct timeb tm;
+  long int seconds;
+  long int milliseconds;
 
   ff_element cell[CELLS];
   ff_element res;
@@ -41,21 +40,45 @@ int main () {
   for (i=0; i<CELLS; i++)
     ff_rand(cell[i]);
 
-  for (op=0; op<2; op++) {
+  for (op=0; op<=6; op++) {
     /* Set timer */
-    //ftime (&tm);
-    //seconds = tm.time;
-    //milliseconds = tm.millitm;
-	gettimeofday(&start, NULL); // Get start time
+    ftime (&tm);
+    seconds = tm.time;
+    milliseconds = tm.millitm;
 
     /* Operation */
     switch(op) {
     case 0:
+      /* Addition */
+      for (i=0; i<ITERATIONS*coeff[op]; i++)
+	ff_add( cell[i%CELLS], cell[(i+CELLS/2)%CELLS], res );
+      break;
+    case 1:
+      /* Squaring */
+      for (i=0; i<ITERATIONS*coeff[op]; i++)
+	ff_square( cell[i%CELLS], res );
+      break;
+    case 2:
       /* Product rlcomb */
       for (i=0; i<ITERATIONS*coeff[op]; i++)
 	ff_mul_rlcomb( cell[i%CELLS], cell[(i+CELLS/2)%CELLS], res );
       break;
-    case 1:
+    case 3:
+      /* Product lrcomb */
+      for (i=0; i<ITERATIONS*coeff[op]; i++)
+	ff_mul_lrcomb( cell[i%CELLS], cell[(i+CELLS/2)%CELLS], res );
+      break;
+    case 4:
+      /* Product lrcomb_w4 */
+      for (i=0; i<ITERATIONS*coeff[op]; i++)
+	ff_mul_lrcomb_w4( cell[i%CELLS], cell[(i+CELLS/2)%CELLS], res );
+      break;
+    case 5:
+      /* Inversion */
+      for (i=0; i<ITERATIONS*coeff[op]; i++)
+	ff_inv( cell[i%CELLS], res );
+      break;
+    case 6:
       /* Division */
       for (i=0; i<ITERATIONS*coeff[op]; i++)
 	ff_div( cell[i%CELLS], cell[(i+CELLS/2)%CELLS], res );
@@ -63,22 +86,34 @@ int main () {
     }
 
     /* Print elapsed time */
-    //ftime(&tm);
-    //seconds = tm.time - seconds;
-    //milliseconds = seconds*1000+(tm.millitm -milliseconds);
-	gettimeofday(&end, NULL);
+    ftime(&tm);
+    seconds = tm.time - seconds;
+    milliseconds = seconds*1000+(tm.millitm -milliseconds);
     switch(op) {
     case 0:
-      printf("Product:   ");
+      printf("Addition:     ");
       break;
     case 1:
+      printf("Squaring:     ");
+      break;
+    case 2:
+      printf("Product-rl:   ");
+      break;
+    case 3:
+      printf("Product-lr:   ");
+      break;
+    case 4:
+      printf("Product-lrw4: ");
+      break;
+    case 5:
+      printf("Inversion:    ");
+      break;
+    case 6:
       printf("Division:     ");
       break;
     }
-    //printf("%.3f seconds elapsed -\t %2.3f microseconds average\n",((double)milliseconds/1000), ((double)milliseconds*1000)/(ITERATIONS*coeff[op]));
-printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    printf("%.3f seconds elapsed -\t %2.3f microseconds average\n",((double)milliseconds/1000), ((double)milliseconds*1000)/(ITERATIONS*coeff[op]));
   }
-  return 0;
 }
 
 
