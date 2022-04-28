547b39332809bb7e9cc4c382332b2bace16ded16
Hiroshi Nishida <nishida@asusa.net>
2022-04-28 09:39:35 -0800

    Add new region calculation methods
    Add functions and macros, etc regarding two new region calculation methods. See HOWTOUSE and gf-bench/*/gf-nishida-region-16/gf-bench,c for more details.

--- gf.c
+++ gf.c
@@ -1,45 +1,20 @@
-/****************************************************************************
-
-Copyright (c) 2015, Hiroshi Nishida and ASUSA Corporation
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-****************************************************************************/
+/*
+ *	All rights revserved by ASUSA Corporation and ASJ Inc.
+ *	Copying any part of this program is strictly prohibited.
+ *	Author: Hiroshi Nishida
+ */
 
 /****************************************************************************
 
-	This library provides simple and fast arithmetic functions
-	in GF(2^16) based on memory lookups.
-	The size of memory allocated for the lookup tables is 768kB.
-	GF16mul() and GF16div() are defined in gf.h. 
+	This program provides simple (and fast?) calculation functions
+	in GF(2^16) that use memory lookup.
+	The size of memory allocated for GF(2^16) is 512kB.
+	GF16mul() and GF16div() are difined in gf.h for speedup. 
 
-	Please see our technical paper gf-nishida-16.pdf or
-	gf-nishida-16-ja.pdf for the details.
-
-	CAUTION!! Never use b = 0 for GF16div(a, b) because it causes
-	segmentation violation.
-	For speedup, the program does not check "(a == 0 || b == 0)".
+	CAUTION!! Never use b = 0 for all division functions div(a, b)
+	because it outputs a non-zero random value.
 
 					Hiroshi Nishida
-					nishida at asusa.net
 
 ****************************************************************************/
 
@@ -47,6 +22,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdlib.h> 
 #include <stdint.h> 
 #include <string.h> 
+#include <errno.h> 
 #include <time.h> 
 #define	_GF_MAIN_
 #include "gf.h"
@@ -75,31 +51,234 @@ void
 GF16init(void)
 {
 	int		i;
-	uint16_t	t;
 	uint32_t	n;
 	
 	// Allocate memory 
 	GF16memL = (uint16_t *)malloc(sizeof(uint16_t) * GF16_SIZE * 4);
-	GF16memH = GF16memL + GF16_SIZE - 1; // Second quarter
+	GF16memH = GF16memL + GF16_SIZE - 1; // Second half
 	GF16memIdx = (int *)malloc(sizeof(int) * GF16_SIZE);
-	GF16memL[0] = t = 1;
+	GF16memL[0] = 1;
 
 	// Set GF16memL and GF16memIdx
-	for (i = 1; i < GF16_SIZE - 1; i++) {
-		n = (uint32_t)t << 1;
-		GF16memL[i] = t = (uint16_t)
+	for (i = 0; i < GF16_SIZE - 1;) {
+		n = (uint32_t)GF16memL[i] << 1;
+		i++;
+		GF16memL[i] = (uint16_t)
 			((n >= GF16_SIZE) ? n ^ GF16_PRIM : n);
-		GF16memIdx[t] = i;
+		GF16memIdx[GF16memL[i]] = i;
 	}
 	GF16memIdx[0] = (GF16_SIZE << 1) - 1;
 	GF16memIdx[1] = 0;
 
-	// Copy first quarter of GF16memL to second quarter
+	// Copy first half of GF16memL to second half
 	memcpy(GF16memH, GF16memL, sizeof(uint16_t) * (GF16_SIZE - 1));
 
 	// Fill remaining space after GF16memH with zero
 	memset(&GF16memL[(GF16_SIZE << 1) - 2], 0,
-		sizeof(uint16_t) * (GF16_SIZE + 1));
-	GF16memL[(GF16_SIZE << 2) - 2] = 0;
+		sizeof(uint16_t) * ((GF16_SIZE << 1) + 2));
 }
 
+/************************************************************
+	For regional calculation
+************************************************************/
+
+// Create table for regional calculation such as:
+//     a * x[i]
+//     a / x[i]
+//     x[i] / a
+// This method may fit L2 cache and will be fast.
+//
+// Args:
+//     a: static value in regional calculation (or coefficient)
+//     type: 0: a * x[i]
+//           1: a / x[i]
+//           2: x[i] / a
+//
+// Return value:
+//     pointer to table or NULL if failed. Free it later.
+//
+// How to use:
+//    For a * x[i],
+//        uint16_t *gf_a = GF16crtRegTbl(a, 0);
+//        for (i = 0; i < N; i++) {
+//            y[i] = gf_a[x[i]]; // This is equal to GFmul(a, x[i]);
+//        }
+//        free(gf_a);
+//
+//    For a / x[i],
+//        uint16_t *gf_a = GF16crtRegTbl(a, 1);
+//        for (i = 0; i < N; i++) {
+//            y[i] = gf_a[x[i]]; // This is equal to GFdiv(a, x[i]);
+//        }
+//        free(gf_a);
+//
+//    For x[i] / a,
+//        uint16_t *gf_a = GF16crtRegTbl(a, 2);
+//        for (i = 0; i < N; i++) {
+//            y[i] = gf_a[x[i]]; // This is equal to GFdiv(x[i], a);
+//        }
+//        free(gf_a);
+//
+uint16_t *
+GF16crtRegTbl(uint16_t a, int type)
+{
+	int		i;
+	uint16_t	*table = NULL, *a_addr;
+
+	// Allocate table
+	if ((table = (uint16_t *)malloc(GF16_SIZE * sizeof(uint16_t)))
+			== NULL) {
+		fprintf(stderr, "Error: %s: malloc: %s\n",
+			__func__, strerror(errno));
+		return NULL;	
+	}
+
+	// Input values
+	switch (type) {
+	case 0: // a * x[i]
+		a_addr = GF16memL + GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < GF16_SIZE; i++) {
+			table[i] = a_addr[GF16memIdx[i]];
+		}
+		break;
+
+	case 1: // a / x[i]
+		a_addr = GF16memH + GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < GF16_SIZE; i++) {
+			table[i] = *(a_addr - GF16memIdx[i]);
+		}
+		break;
+
+	case 2: // x[i] / a
+		a_addr = GF16memH - GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < GF16_SIZE; i++) {
+			table[i] = a_addr[GF16memIdx[i]];
+		}
+		break;
+
+	default:
+		fprintf(stderr, "Error: %s: Illegal second argument value: %d "
+			"(value must be 0, 1, or 2)\n",
+			__func__, type);
+		free(table);
+		return NULL;
+	}
+
+	return table;
+}
+
+// Create 8bit split tables for regional calculation such as:
+//     a * x[i]
+//     a / x[i]
+//     x[i] / a
+// This method may fit L1 cache and will be even faster.
+//
+// Args:
+//     a: static value in regional calculation (or coefficient)
+//     type: 0: a * x[i]
+//           1: a / x[i]
+//           2: x[i] / a
+//
+// Return value:
+//     pointer to lowest byte table or NULL if failed. Free it later.
+//
+// How to use:
+//    For a * x[i],
+//        uint16_t *gf_a_l = GF16crtSpltRegTbl(a, 0);
+//        uint16_t *gf_a_h = gf_a_l + 256;
+//        uint16_t xi;
+//        for (i = 0; i < N; i++) {
+//            xi = x[i];
+//            y[i] = gf_a_h[xi >> 8] ^ gf_a_l[xi & 0xff]; // = GFmul(a, x[i]);
+//            // Or use y[i] = GF16SRT(gf_a_l, gf_a_h, xi);
+//        }
+//        free(gf_a_l);
+//
+//    For a / x[i],
+//        uint16_t *gf_a_l = GF16crtSpltRegTbl(a, 1);
+//        uint16_t *gf_a_h = gf_a_l + 256;
+//        uint16_t xi;
+//        for (i = 0; i < N; i++) {
+//            xi = x[i];
+//            y[i] = gf_a_h[xi >> 8] ^ gf_a_l[xi & 0xff]; // = GFdiv(a, x[i]);
+//            // Or use y[i] = GF16SRT(gf_a_l, gf_a_h, xi);
+//        }
+//        free(gf_a_l);
+//
+//    For x[i] / a,
+//        uint16_t *gf_a_l = GF16crtSpltRegTbl(a, 1);
+//        uint16_t *gf_a_h = gf_a_l + 256;
+//        uint16_t xi;
+//        for (i = 0; i < N; i++) {
+//            xi = x[i];
+//            y[i] = gf_a_h[xi >> 8] ^ gf_a_l[xi & 0xff]; // = GFdiv(x[i], a);
+//            // Or use y[i] = GF16SRT(gf_a_l, gf_a_h, xi);
+//        }
+//        free(gf_a_l);
+//
+uint16_t *
+GF16crtSpltRegTbl(uint16_t a, int type)
+{
+	int		i;
+	uint16_t	*tb_h, *tb_l, *a_addr;
+
+	// Initialize
+	tb_l = NULL;
+
+	// Allocate table
+	if ((tb_l = (uint16_t *)malloc(256 * sizeof(uint16_t) * 2)) == NULL) {
+		fprintf(stderr, "Error: %s: malloc: %s\n",
+			__func__, strerror(errno));
+		return NULL;
+	}
+	tb_h = tb_l + 256;
+	memset(tb_l, 0, 256 * sizeof(uint16_t) * 2);
+
+	// Input values
+	switch (type) {
+	case 0: // a * x[i]
+		a_addr = GF16memL + GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < 256; i++) {
+			tb_l[i] = a_addr[GF16memIdx[i]];
+			tb_h[i] = a_addr[GF16memIdx[i << 8]];
+		}
+		break;
+
+	case 1: // a / x[i]
+		a_addr = GF16memH + GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < 256; i++) {
+			tb_l[i] = *(a_addr - GF16memIdx[i]);
+			tb_h[i] = *(a_addr - GF16memIdx[i << 8]);
+		}
+		break;
+
+	case 2: // x[i] / a
+		a_addr = GF16memH - GF16memIdx[a];
+
+		// Input values
+		for (i = 0; i < 256; i++) {
+			tb_l[i] = a_addr[GF16memIdx[i]];
+			tb_h[i] = a_addr[GF16memIdx[i << 8]];
+		}
+		break;
+
+	default:
+		fprintf(stderr, "Error: %s: Illegal second argument value: %d "
+			"(value must be 0, 1, or 2)\n",
+			__func__, type);
+		free(tb_l);
+		return NULL;
+	}
+
+	return tb_l;
+}
--- gf.h
+++ gf.h
@@ -1,54 +1,44 @@
 #ifndef _GF_H_
 #define _GF_H_
 
-/****************************************************************************
-
-Copyright (c) 2015, Hiroshi Nishida and ASUSA Corporation
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-****************************************************************************/
-
-/****************************************************************************
-
-	This program provides simple and fast arithmetic functions
-	in GF(2^16) based on memory lookups.
-	The size of memory allocated for GF(2^16) is 768kB.
-	GF16mul() and GF16div() are difined in gf.h for speedup. 
-
-	Please see our technical paper (gf-nishida-16.pdf) for the details.
-
-	CAUTION!! Never use b = 0 for GF16div(a, b) because it causes
-	segmentation violation.
-	For speedup, code like "(!a || !b) ? 0 : ..." has been removed.
-
-                                        Hiroshi Nishida
-
-****************************************************************************/
-
 #include <stdint.h>
+
+/****************************************************************************
+
+	Simple (and fast?) multiplication and division functions in
+	GF(2^16) that use memory lookup(s).
+	Memory consumptions are as follows:
+		GF16mul(), GF16div(): 768kB
+		GF16crtRegTbl: 128kB (may fit L2 cache)
+		GF16crtSpltRegTbl: 1kB (may fit L1 cache)
+
+	CAUTION!! Never use b = 0 for disvision (e.g. GF16div(a, b))
+	as it will output a wrong value.
+	For speedup, we don't check if a, b == 0.
+
+					Hiroshi Nishida
+        
+****************************************************************************/
 
 /************************************************************
 	Definitions
 ************************************************************/
+
+// To achieve fast computation, we do not check if a, b == 0
+// CAUTION: DO NOT USE b = 0 for GF16div(a, b). IT DOES NOT WORK CORRECTLY.
+#define	GF16mul(a, b)	(GF16memL[GF16memIdx[(a)] + GF16memIdx[(b)]])
+#define	GF16div(a, b)	(GF16memH[GF16memIdx[(a)] - GF16memIdx[(b)]])
+
+// Some macros for simplification 
+#define GF16crtRegTblMul(a)	GF16crtRegTbl(a, 0)
+#define GF16crtRegTblDivL(a)	GF16crtRegTbl(a, 1)
+#define GF16crtRegTblDivR(a)	GF16crtRegTbl(a, 2)
+#define GF16crtSpltRegTblMul(a)		GF16crtSpltRegTbl(a, 0)
+#define GF16crtSpltRegTblDivL(a)	GF16crtSpltRegTbl(a, 1)
+#define GF16crtSpltRegTblDivR(a)	GF16crtSpltRegTbl(a, 2)
+
+#define GF16RT(gf_a, x)		gf_a[(x)]
+#define GF16SRT(gf_a_l, gf_a_h, x)	gf_a_h[(x) >> 8] ^ gf_a_l[(x) & 0xff]
 
 /************************************************************
 	Variables
@@ -72,13 +62,9 @@ extern int	*GF16memIdx;
 	Functions
 ************************************************************/
 
-// Initialization
-void	GF16init(void); 
-
-// Multiplication and division
-// CAUTION: DO NOT USE b = 0 for GF16div(a, b).  IT DOES NOT WORK.
-#define	GF16mul(a, b)	(GF16memL[GF16memIdx[(a)] + GF16memIdx[(b)]])
-#define	GF16div(a, b)	(GF16memH[GF16memIdx[(a)] - GF16memIdx[(b)]])
-
+// 16bit
+void		GF16init(void); 
+uint16_t	*GF16crtRegTbl(uint16_t, int);
+uint16_t	*GF16crtSpltRegTbl(uint16_t, int);
 
 #endif // _GF_H_
